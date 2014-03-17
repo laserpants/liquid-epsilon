9fb01a358d0778f357fdf3557e005710eece347a
johanneshilden <hildenjohannes@gmail.com>
2014-03-17 14:39:00 +0100

    added MonadPlus and MonadState instances to State monad

--- Util/Monad/State.hs
+++ Util/Monad/State.hs
@@ -1,5 +1,7 @@
 module Util.Monad.State where
 
+import Control.Monad
+
 newtype State s a = State { runState :: (s -> (a, s)) }
  
 instance Monad (State s) where
@@ -8,6 +10,18 @@ instance Monad (State s) where
         let (v, s') = x s
         in  runState (f v) s'
 
+class Monad m => MonadState s m | m -> s where
+    get :: m s
+    get = state (\s -> (s, s))
+    put :: s -> m ()
+    put s = state (\_ -> ((), s))
+    state :: (s -> (a, s)) -> m a
+    state f = do
+        s <- get
+        let ~(a, s') = f s
+        put s'
+        return a
+
 newtype StateT s m a = StateT { runStateT :: (s -> m (a, s)) }
 
 instance (Monad m) => Monad (StateT s m) where
@@ -15,4 +29,12 @@ instance (Monad m) => Monad (StateT s m) where
     (StateT x) >>= f = StateT $ \s -> do
         (v, s') <- x s          
         runStateT (f v) s'    
+
+instance (Monad m) => MonadState s (StateT s m) where
+    get   = StateT $ \s -> return (s, s)
+    put s = StateT $ \_ -> return ((), s)
+
+instance (MonadPlus m) => MonadPlus (StateT s m) where
+    mzero = StateT $ \s -> mzero
+    (StateT x1) `mplus` (StateT x2) = StateT $ \s -> (x1 s) `mplus` (x2 s)
 
